57d56d3630f531b5b70f144ff43bf2637e1046e9
jalbe <jalbertm@gmail.com>
2022-11-23 17:49:03 +0000

    version 10

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("Hola mundo 7!");
         System.out.println("Hola mundo 8!");
         System.out.println("Hola mundo 9!");
+        System.out.println("Hola mundo 10!");
 
     }
 }
